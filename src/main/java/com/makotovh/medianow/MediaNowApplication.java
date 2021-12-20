ed78139f4899e3844ea533005c8082fa11d4296b
makotovh <makotovh@gmail.com>
2021-12-20 12:22:30 +0100

    Initial commit

---
+++ src/main/java/com/makotovh/medianow/MediaNowApplication.java
@@ -0,0 +1,13 @@
+package com.makotovh.medianow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaNowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediaNowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/makotovh/medianow/MediaNowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makotovh.medianow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaNowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
